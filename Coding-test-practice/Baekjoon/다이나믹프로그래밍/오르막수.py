e81e353a733f978dffe2cb02960c856eead4d794
s00hyun <ff152@gist.ac.kr>
2020-03-17 23:19:48 +0900

    Solve BFS and DP Problems

--- Coding-test-practice/Baekjoon/다이나믹프로그래밍/오르막수.py
+++ Coding-test-practice/Baekjoon/다이나믹프로그래밍/오르막수.py
@@ -7,7 +7,8 @@ def ans(n):
         return sum(D[1])
     for i in range(2, n+1):
         for j in range(10):
+            # D[i][j]: j로 시작하는 i자리 오르막 수의 갯수
             D[i][j] = sum(D[i-1][:j+1]) % 10007
-    return sum(D[N]) % 10007
+    return sum(D[n]) % 10007
 
 print(ans(N) % 10007)
