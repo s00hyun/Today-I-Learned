402c6eed4581d636cc31919f8c2e4ffda537c628
s00hyun <ff152@gist.ac.kr>
2020-04-03 23:30:53 +0900

    Solve a binary search problem

--- Coding-test-practice/Programmers/Kakao/징검다리건너기.py
+++ Coding-test-practice/Programmers/Kakao/징검다리건너기.py
@@ -4,25 +4,33 @@
 def solution(stones, k):
     # 돌다리를 건널 수 있는 놈들의 최댓값(m)을 이분탐색으로 찍는다
     left = 0
-    right = max(stones)
-    while left <= right:
-        max_0 = 0
-        temp = []
+    right = 200000000
+    max_0 = 0
+    while left < right:
+        print("left:{}, right:{}".format(left, right))
         m = (left + right) // 2
-        for s in stones:
-            temp.append(max(0, s - (m - 1)))
-        print("m:", m, "temp:", temp)
-        for i in range(len(temp) - k):
-            cnt = temp[i:i+3].count(0)
-            max_0 = max(max_0, cnt)
-            print("cnt:", cnt, "max_0:", max_0)
-        if max_0 < k:
+        cnt = 0
+        fl=0
+
+        print("m:", m)
+        for i in range(len(stones)):
+            if stones[i] - m <= 0:
+                cnt += 1
+                if cnt >= k:
+                    fl=1
+                    print("cnt1:", cnt)
+                    right = m
+                    break
+            else:
+                cnt = 0
+        if fl==0:
+            print("cnt2:", cnt)
+            max_0 = max(max_0, m+1)
+            print("max_0:", max_0)
             left = m + 1
-        elif max_0 > k:
-            right = m - 1
-        else:
-            return m - 1
-    return m - 1
 
+    return max_0
 
-print(solution([2, 3, 1, 4, 6], 1))
+#print(solution([2, 4, 5, 3, 2, 1, 4, 2, 5, 1], 3))
+#print(solution([2, 3, 1, 4, 6], 1))
+print(solution([3,3,3,3], 1))
