102035df1264cc0590b9013e6c4df280f066f73c
s00hyun <ff152@gist.ac.kr>
2020-04-17 00:32:53 +0900

    Solve 2048(Easy)

--- Coding-test-practice/Baekjoon/삼성SW역량기출/2048(Easy).py
+++ Coding-test-practice/Baekjoon/삼성SW역량기출/2048(Easy).py
@@ -1,145 +1,108 @@
-# 실패
-# 각 step마다 board 정보를 저장하고 있어야 하는데, 단순 DFS로는 불가능..?
+# 각 step마다 board 정보를 저장하고 있어야 한다
 # 재귀로 다시 짜기
 
-import sys, copy
+import sys
+from collections import deque
 
 n = int(sys.stdin.readline().rstrip())
 board = [ list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(n) ]
-check = [[-1] * n for _ in range(n)]
+answer = 0
+q = deque()
 #print(board)
 
-for i in range(n):
-    for j in range(n):
-        if board[i][j] != 0:
-            check[i][j] = 1
-#print(check)
+board_copy = [ x[:] for x in board ]
+#print("board_copy:", board_copy)
 
 
-def move(d, s, _board, _check):
-    #print("####", (d, s))
-    for i in range(n):
-        arr = []
-        if d == 1:  # 상
-            arr = [ _board[j][i] for j in range(n) ]
-            chk = [ _check[j][i] for j in range(n)  ]
-        elif d == 2:  # 하
-            arr = list(reversed([ _board[j][i] for j in range(n) ]))
-            chk = list(reversed([ _check[j][i] for j in range(n) ]))
-        elif d == 3:  # 좌
-            arr = [ _board[i][j] for j in range(n) ]
-            chk = [ _check[i][j] for j in range(n) ]
-        elif d == 4:  # 우
-            arr = list(reversed([ _board[i][j] for j in range(n) ]))
-            chk = list(reversed([ _check[i][j] for j in range(n) ]))
+def get_num(i, j):
+    if board_copy[i][j] != 0:
+        q.append(board_copy[i][j])
+        board_copy[i][j] = 0
 
-        # 블록을 차례대로 비교하기 전에 블록을 한 방향으로 모아야 함!!!
-        # 테스트케이스: 그림2 -> 그림3
-        arr = [val for val in arr if val != 0]
-        chk = [val for val in chk if val != -1]
-        #print("arr:", arr)
-        if d == 1 or d == 3:
-            arr.extend([0] * (n - len(arr)))
-            chk.extend([-1] * (n - len(chk)))
-        elif d == 2 or d == 4:
-            temp = [0] * (n - len(arr))
-            temp.extend(list(reversed(arr)))
-            arr = copy.deepcopy(temp)
-            temp = [-1] * (n - len(chk))
-            temp.extend(list(reversed(chk)))
-            chk = copy.deepcopy(temp)
-        print("moved arr:", arr)
-        print("moved chk:", chk)
 
-        # 합칠 블록이 있는지 체크
-        match = 0
-        for idx in range(n-1):
-            if arr[idx] == arr[idx+1] and chk[idx] == 1:
-                match = 1
-                arr[idx+1] *= 2
-                arr[idx] = -1  # 삭제할 위치 표시
-                chk[idx] = 2  # 삭제할 위치 표시
-                chk[idx+1] = 0
-        #print(arr)
-
-        # 합칠 블록이 없을 경우, 해당 step을 위해 복사된 board와 check 배열에 이동된 블록을 업데이트한다.
-        if not match:
-            #print("not match")
-            for j in range(n):
-                if d == 1:
-                    _board[j][i] = arr[j]
-                    _check[j][i] = chk[j]
-                elif d == 2:
-                    _board[j][i] = arr[(n - 1) - j]
-                    _check[j][i] = chk[(n - 1) - j]
-                elif d == 3 or d == 4:
-                    _board[i] = arr
-                    _check[i] = chk
-            continue
-
-        # 합칠 블록이 있을 경우, 합친 후, 해당 step을 위해 복사된 board와 check 배열에 블록을 업데이트한다.
-        arr = [ val for val in arr if val != -1 ]
-        chk = [ val for val in chk if val != 2 ]
-
-        if d == 1 or d == 3:
-            arr.extend([0] * (n - len(arr)))
-            chk.extend([-1] * (n - len(chk)))
-        elif d == 2 or d == 4:
-            temp = [0] * (n - len(arr))
-            temp.extend(arr)
-            arr = copy.deepcopy(temp)
-            temp = [-1] * (n - len(chk))
-            temp.extend(chk)
-            chk = copy.deepcopy(temp)
-        print("match:", match, "arr:", arr, "chk:", chk)
-
+def move(d):
+    if d == 1:  # 상
         for j in range(n):
-            if d == 1:
-                _board[j][i] = copy.deepcopy(arr[j])
-                _check[j][i] = copy.deepcopy(chk[j])
-            elif d == 2:
-                _board[j][i] = copy.deepcopy(arr[(n-1)-j])
-                _check[j][i] = copy.deepcopy(chk[(n-1)-j])
-            elif d == 3 or d == 4:
-                _board[i] = copy.deepcopy(arr)
-                _check[i] = copy.deepcopy(chk)
-
-
-    if s == 2:  # For debugging
-    #if s == 5:
-        print("vertex:", (d,s), "_board:", _board)
-        max_val = 0
+            for i in range(n):
+                get_num(i, j)
+            i = 0
+            while q:
+                v = q.popleft()
+                if board_copy[i][j] == 0:
+                    board_copy[i][j] = v
+                elif board_copy[i][j] == v:  # 합칠 수 있을 경우
+                    board_copy[i][j] *= 2
+                    i += 1
+                else:  # 합칠 수 없을 경우
+                    i += 1
+                    board_copy[i][j] = v
+    elif d == 2:  # 하
+        for j in range(n):
+            for i in range(n-1, -1, -1):  # 밑에서부터 읽어서 큐에 넣음
+                get_num(i, j)
+            i = n-1
+            while q:
+                v = q.popleft()
+                if board_copy[i][j] == 0:
+                    board_copy[i][j] = v
+                elif board_copy[i][j] == v:  # 합칠 수 있을 경우
+                    board_copy[i][j] *= 2
+                    i -= 1
+                else:  # 합칠 수 없을 경우
+                    i -= 1
+                    board_copy[i][j] = v
+    elif d == 3:  # 좌
         for i in range(n):
-            #print("max(_board[i])", max(_board[i]))
-            max_val = max(max_val, max(_board[i]))
-        #print("max_val:", max_val)
-        return max_val
-    else:
-        #if d == 3:
-        #    print("vertex:", (d,s), "_board:", _board)
-        return 0
+            for j in range(n):
+                get_num(i, j)
+            j = 0
+            while q:
+                v = q.popleft()
+                if board_copy[i][j] == 0:
+                    board_copy[i][j] = v
+                elif board_copy[i][j] == v:  # 합칠 수 있을 경우
+                    board_copy[i][j] *= 2
+                    j += 1
+                else:  # 합칠 수 없을 경우
+                    j += 1
+                    board_copy[i][j] = v
+    elif d == 4:  # 우
+        for i in range(n):
+            for j in range(n-1, -1, -1):
+                get_num(i, j)
+            j = n-1
+            while q:
+                v = q.popleft()
+                if board_copy[i][j] == 0:
+                    board_copy[i][j] = v
+                elif board_copy[i][j] == v:  # 합칠 수 있을 경우
+                    board_copy[i][j] *= 2
+                    j -= 1
+                else:  # 합칠 수 없을 경우
+                    j -= 1
+                    board_copy[i][j] = v
+
+    #print("board_copy:", board_copy)
 
 
-def dfs():
+def solve(step):
     # 상: 1, 하: 2, 좌:3, 우:4
-    stack = [(4, 1), (3, 1), (2, 1), (1, 1)]
-    board_copy = copy.deepcopy(board)
-    check_copy = copy.deepcopy(check)
-    answer = 0
-    count = []
+    global board_copy, answer
 
-    while stack:
-        direction, step = stack.pop()
-        count.append(move(direction, step, board_copy, check_copy))
-        if step == 5:
-            board_copy = copy.deepcopy(board)
-            check_copy = copy.deepcopy(check)
-            answer = max(answer, max(count))
-        elif step < 5:
-            stack.extend( list(reversed([ (i, step+1) for i in range(1, 5) ])) )
+    if step == 5:
+        # 최댓값 계산
+        for i in range(n):
+            answer = max(answer, max(board_copy[i]))
+        return
 
-    return answer
+    ### 중요:
+    # 다음 step을 탐색한 후 현재 step으로 돌아가는 경우도 있으므로, board를 복사해둔다.
+    board_copy2 = [x[:] for x in board_copy]  # 부모 노드에서의 보드 정보 (!지역변수!)
+    for direction in range(1, 5):
+        move(direction)  # board_copy 사용
+        solve(step + 1)  # step 끝까지 갔는지 체크
+        board_copy = [x[:] for x in board_copy2]  # 자식 노드들은 모두 부모 노드의 보드를 이용해 연산을 수행해야 한다.
 
 
-#print(dfs())
-move(1, 1, board, check)
+solve(0)
+print(answer)
