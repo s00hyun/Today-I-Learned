de50e73c49f25777708b59a8ba6fba59661257e2
s00hyun <ff152@gist.ac.kr>
2020-05-01 21:59:28 +0900

    Add comments on BOJ14891 (톱니바퀴)

--- Coding-test-practice/Baekjoon/삼성SW역량기출/톱니바퀴.py
+++ Coding-test-practice/Baekjoon/삼성SW역량기출/톱니바퀴.py
@@ -19,14 +19,19 @@ def rotate(_i, _d):
 def go(_n, _r):
     global wheel
     check = [ [0]*5 for _ in range(5) ]
+    # 맞물린 톱니의 숫자가 서로 다른 톱니바퀴 쌍을 2차원 배열에 저장한다
     for i in range(1, 4):
         if wheel[i][2] != wheel[i+1][6]:
             check[i][i+1] = 1
             check[i+1][i] = 1
+    # 톱니바퀴를 회전시킨다
     rotate(_n, _r)
+    # 회전한 톱니바퀴의 왼쪽에 있는 톱니바퀴들 중에 돌릴 수 있는 것은 모두 돌린다
+    # (맨 오른쪽 톱니바퀴를 돌렸을 때, 맨 왼쪽 톱니바퀴까지 모두 돌아갈 수 있는 경우를 생각 (예제 3))
     if _n-1 >= 1 and check[_n][_n-1] and not check_rotated[_n-1]:
         #print("go again", _n - 1, -1 * _r)
         go(_n-1, -1*_r)
+    # 회전한 톱니바퀴의 오른쪽에 있는 톱니바퀴들 중에 돌릴 수 있는 것은 모두 돌린다
     if _n+1 <= 4 and check[_n][_n+1] and not check_rotated[_n+1]:
         #print("go again", _n + 1, -1 * _r)
         go(_n+1, -1*_r)
