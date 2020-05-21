b201ebab87adce0bb57224862c8f1e56a2b4267f
s00hyun <ff152@gist.ac.kr>
2020-05-21 23:32:39 +0900

    Solve algorithm problems from Programmers

--- Coding-test-practice/Programmers/Stack-queue/프린터.py
+++ Coding-test-practice/Programmers/Stack-queue/프린터.py
@@ -1,5 +1,27 @@
 from collections import deque
 
+
+# new code
+def solution(priorities, location):
+    pool = [(i, pri) for i, pri in enumerate(priorities)]
+    q = deque(pool)
+    answer = 0
+    if len(priorities) == 1:
+        return 1
+
+    while True:
+        maxp = max([x[1] for x in q])
+        p = q.popleft()
+        if p[1] < maxp:  # 나머지 인쇄 대기목록에서 J보다 중요도가 높은 문서가 한 개라도 존재함
+            q.append(p)
+            # print(q)
+        else:
+            answer += 1
+            if p[0] == location:
+                return answer
+
+
+'''
 def solution(priorities, location):
     c = 0
     index = deque()
@@ -16,13 +38,11 @@ def solution(priorities, location):
                 index.append(left_i)
                 priorities.append(left)
             else: 
-                #print("max!")
                 c += 1
                 if left_i == location :
                     return c
-            #print("priorities =", priorities)
-            #print("index =", index)
-            #print(c)
+'''
+
 
 sol1 = solution([2,1,3,2], 2)
 print(sol1)
